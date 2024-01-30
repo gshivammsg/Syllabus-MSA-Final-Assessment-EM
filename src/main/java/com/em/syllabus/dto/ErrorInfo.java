2ef3044dc477acb91edc5a5fea2e824b6f5c17e3
shivamgupta <e29748@extramark.onmicrosoft.com>
2024-01-30 10:40:02 +0530

    ADDED Global Exception Handler

---
+++ src/main/java/com/em/syllabus/dto/ErrorInfo.java
@@ -0,0 +1,19 @@
+package com.em.syllabus.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * The type Error info.
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorInfo {
+    private String field;
+    private String message;
+}
+
--- src/main/java/com/em/syllabus/dto/request_dto/SyllabusRequestDTO.java
+++ src/main/java/com/em/syllabus/dto/request_dto/SyllabusRequestDTO.java
@@ -1,9 +1,9 @@
 package com.em.syllabus.dto.request_dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+
 
 @Getter
 @Setter
@@ -16,19 +16,19 @@ public class SyllabusRequestDTO {
     @JsonProperty("id")
     private Integer id;
 
-    @NotEmpty(message = "syllabus_name cannot be empty")
+    @NotEmpty()
     @JsonProperty("syllabus_name")
     private String syllabusName;
 
-    @NotEmpty(message = "description cannot be empty")
+    @NotEmpty()
     @JsonProperty("description")
     private String description;
 
-    @NotEmpty(message = "topics cannot be empty")
+    @NotEmpty()
     @JsonProperty("topics")
     private String topics;
 
-    @NotEmpty(message = "duration cannot be empty")
+    @NotEmpty()
     @JsonProperty("duration")
     private String duration;
 }
